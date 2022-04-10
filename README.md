fb45dacaeef42bd79f1f95a413ad3534a65ae0c0
Austin <austinlasseter@gmail.com>
2022-04-10 09:16:24 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# 502-california-housing-regression
+## View the finished model:
+* https://ames-housing-linear.herokuapp.com/ 
+
+## Read this blog post
+* [How to deploy a Plotly Dash app on Heroku](https://austinlasseter.medium.com/deploy-a-plotly-dash-app-on-heroku-4d2c3224230)
+
+
+### Additional Reading
+* Plotly’s [Dash deployment guide](https://dash.plotly.com/deployment)
+* Heroku’s [deployment guide](https://devcenter.heroku.com/articles/getting-started-with-python)
+* Fantastic [blog post](https://towardsdatascience.com/deploying-your-dash-app-to-heroku-the-magical-guide-39bd6a0c586c) that dives deep
+* Excellent [YouTube tutorial](https://www.youtube.com/watch?v=b-M2KQ6_bM4&feature=youtu.be)
---
+++ app.py
@@ -0,0 +1,95 @@
+import dash
+from dash import dcc,html
+from dash.dependencies import Input, Output, State
+
+
+
+########### Define your variables ######
+myheading1='Predicting Home Sale Prices in Ames, Iowa'
+image1='ames_welcome.jpeg'
+tabtitle = 'Ames Housing'
+sourceurl = 'http://jse.amstat.org/v19n3/decock.pdf'
+githublink = 'https://github.com/plotly-dash-apps/501-linear-reg-ames-housing'
+
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Set up the layout
+app.layout = html.Div(children=[
+    html.H1(myheading1),
+    html.Div([
+        html.Img(src=app.get_asset_url(image1), style={'width': '30%', 'height': 'auto'}, className='four columns'),
+        html.Div([
+                html.H3('Features of Home:'),
+                html.Div('Year Built:'),
+                dcc.Input(id='YearBuilt', value=2010, type='number', min=2006, max=2010, step=1),
+                html.Div('Bathrooms:'),
+                dcc.Input(id='Bathrooms', value=2, type='number', min=1, max=5, step=1),
+                html.Div('Bedrooms:'),
+                dcc.Input(id='BedroomAbvGr', value=4, type='number', min=1, max=5, step=1),
+                html.Div('Total Square Feet:'),
+                dcc.Input(id='TotalSF', value=2000, type='number', min=100, max=5000, step=1),
+                html.Div('Single Family Home:'),
+                dcc.Input(id='SingleFam', value=0, type='number', min=0, max=1, step=1),
+                html.Div('Large Neighborhood:'),
+                dcc.Input(id='LargeNeighborhood', value=0, type='number', min=0, max=1, step=1),
+
+            ], className='four columns'),
+            html.Div([
+                html.Button(children='Submit', id='submit-val', n_clicks=0,
+                                style={
+                                'background-color': 'red',
+                                'color': 'white',
+                                'margin-left': '5px',
+                                'verticalAlign': 'center',
+                                'horizontalAlign': 'center'}
+                                ),
+                html.H3('Predicted Home Value:'),
+                html.Div(id='Results')
+            ], className='four columns')
+        ], className='twelve columns',
+    ),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.H4('Regression Equation:'),
+    html.Div('Predicted Price = (- $1,360.5K Baseline) + ($0.7K * Year Built) + ($12.7K * Bathrooms) + (- $7.7K * Bedrooms) + ($0.049K * Total Square Feet) + ($ 25.2K * Single Family Home) + (- $6.6 K * Large Neighborhood)'),
+    html.Br(),
+    html.A('Google Spreadsheet', href='https://docs.google.com/spreadsheets/d/1q2ustRvY-GcmPO5NYudvsBEGNs5Na5p_8LMeS4oM35U/edit?usp=sharing'),
+    html.Br(),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
+    ]
+)
+
+
+######### Define Callback
+@app.callback(
+    Output(component_id='Results', component_property='children'),
+    Input(component_id='submit-val', component_property='n_clicks'),
+    State(component_id='YearBuilt', component_property='value'),
+    State(component_id='Bathrooms', component_property='value'),
+    State(component_id='BedroomAbvGr', component_property='value'),
+    State(component_id='TotalSF', component_property='value'),
+    State(component_id='SingleFam', component_property='value'),
+    State(component_id='LargeNeighborhood', component_property='value')
+
+)
+def ames_lr_function(clicks, YearBuilt,Bathrooms,BedroomAbvGr,TotalSF,SingleFam,LargeNeighborhood):
+    if clicks==0:
+        return "waiting for inputs"
+    else:
+        y = [-1360501.3809 + 704.4287*YearBuilt + 12738.4775*Bathrooms + -7783.1712*BedroomAbvGr + 49.824*TotalSF+ 25282.091*SingleFam+ -6637.2636*LargeNeighborhood]
+        formatted_y = "${:,.2f}".format(y[0])
+        return formatted_y
+
+
+
+############ Deploy
+if __name__ == '__main__':
+    app.run_server(debug=True)
